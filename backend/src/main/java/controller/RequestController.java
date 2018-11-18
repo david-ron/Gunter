23b7864d22bff5352c9234a0944bb81560600a1d
Raymand <yongconglei@gmail.com>
2018-11-17 21:42:47 -0500

    [topic] add template method for translate

--- backend/src/main/java/controller/RequestController.java
+++ backend/src/main/java/controller/RequestController.java
@@ -4,7 +4,12 @@ package controller;
 import model.Resume;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
 
 @RestController
 public class RequestController {
@@ -22,10 +27,17 @@ public class RequestController {
 
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public void translate(@RequestBody Resume resume){
+    public @ResponseBody ResponseEntity translate(@RequestBody Resume resume){
         //TODO
 
+        String filename = "test.pdf";
+        byte[] out = new byte[10];
 
-        return;
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("content-disposition", "attachment; filename=" + filename);
+        responseHeaders.add("Content-Type", "pdf");
+
+
+        return new ResponseEntity(out, responseHeaders, HttpStatus.OK);
     }
 }
