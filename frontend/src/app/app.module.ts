baa1199c047de0668284273482cefbd5e57a6138
Raymand <yongconglei@gmail.com>
2018-11-17 23:43:05 -0500

    [topic] add primeng dependency

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {CheckboxModule} from 'primeng/checkbox';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {InputTextModule} from 'primeng/inputtext';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CheckboxModule,
+    RadioButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
